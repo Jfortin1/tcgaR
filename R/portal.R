f5c813cdc98f099f1855dc75240c69317a86382a
jfortin <jfortin@jhsph.edu>
2014-10-09 13:38:36 -0400

    Added what option

--- R/portal.R
+++ R/portal.R
@@ -3,15 +3,16 @@ library(downloader)
 
 
 
-	getTCGA <- function(cancer, datatype = c("methylation"), platform = c("27k", "450k"), verbose=FALSE){
+	getTCGA <- function(cancer, datatype = c("methylation"), platform = c("27k", "450k"), verbose=FALSE, what = c("both", "normal","tumor")){
 		cancer <- tolower(cancer)	
 		datatype <- match.arg(datatype)
 		platform <- match.arg(platform)
+		what <- match.arg(what)
 
 		if (datatype != "methylation"){
 			stop("Only methylation data are supported for the moment")
 		}
-		rgset <- getTCGA.meth(cancer = cancer, platform = platform, verbose = verbose)
+		rgset <- getTCGA.meth(cancer = cancer, platform = platform, verbose = verbose, what = what)
 		rgset
 
 	}

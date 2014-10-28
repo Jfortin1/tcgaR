04c34ab415c8837d14fdadd64af1f33e6b075456
jfortin <jfortin@jhsph.edu>
2014-10-28 13:20:11 -0400

    Updated processBarcodes.R

--- R/utils.R
+++ R/utils.R
@@ -3,16 +3,31 @@
 
 #data <- getTCGA.expression("kirc", "genes")
 
-.processBarcode <- function(barcodes){
+.processBarcodes <- function(barcodes){
 	tss <- substr(barcodes, 6, 7)
 	participant <- substr(barcodes, 9, 12)
-	sample.type.num <- substr(barcodes, 14, 15)
-	sample.type <- 
+	sample.type <- substr(barcodes, 14, 15)
+	sample.type <- as.numeric(as.character(sample.type))
 	center <- substr(barcodes, 27, 28)
-	data.frame(tss=tss, participant = participant, sample.type = sample.type, center = center)
-
+	sampleType <- read.csv(file.path("/Users/Jean-Philippe/tcgaR/data/", "sampleType.csv"))
+	sample.code <- sample.type
+	sample.type <- as.character(sampleType$Definition[match(as.character(sample.type), sampleType$Code)])
+	data.frame(tss=tss, participant = participant, sample.type = sample.type, sample.code = sample.code, center = center)
 }
 
+#Tumor types range from 01 - 09, 
+#normal types from 10 - 19 and 
+#control samples from 20 - 29. 
+
 #a <- .processBarcode(barcode.meth)
+
+mappings <- .getMethMappings("brca", "27k")
+barcodes <- mappings$TCGA.ID 
+barcodes <- .processBarcodes(barcodes)
+
+
+
+
+
 
 

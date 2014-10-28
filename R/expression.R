ef2859407b6a2223ed7e299638c08becac781f99
jfortin <jfortin@jhsph.edu>
2014-10-28 12:41:51 -0400

    Moved process barcodes

--- R/expression.R
+++ R/expression.R
@@ -4,22 +4,9 @@ library(downloader)
 
 
 
-data <- getTCGA.expression("kirc", "genes")
-
-link <- "https://tcga-data.nci.nih.gov/datareports/codeTablesReport.htm?codeTable=Tissue%20Source%20Site"
 
 
-.processBarcode <- function(barcodes){
-	tss <- substr(barcodes, 6, 7)
-	participant <- substr(barcodes, 9, 12)
-	sample.type.num <- substr(barcodes, 14, 15)
-	sample.type <- 
-	center <- substr(barcodes, 27, 28)
-	data.frame(tss=tss, participant = participant, sample.type = sample.type, center = center)
 
-}
-
-a <- .processBarcode(barcode.meth)
 
 getTCGA.expression <- function(cancer, platform = c("genes","junctions","isoforms","genes.normalized","isoforms.normalized", "exons"), what = c("both", "normal", "cancer"), verbose = FALSE){
 		platform <- match.arg(platform)

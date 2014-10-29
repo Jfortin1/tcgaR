ebe2711e22c52db6559f31359840ae6984d4250c
jfortin <jfortin@jhsph.edu>
2014-10-29 13:40:56 -0400

    Updated portal for gene expression

--- R/portal.R
+++ R/portal.R
@@ -3,18 +3,21 @@ library(downloader)
 
 
 
-getTCGA <- function(cancer, datatype = c("methylation"), platform = c("27k", "450k"), verbose=FALSE, what = c("both", "normal","tumor")){
+getTCGA <- function(cancer, datatype = c("methylation", "expression"), platform = c("27k", "450k"), verbose=FALSE){
 	cancer <- tolower(cancer)	
 	datatype <- match.arg(datatype)
 	platform <- match.arg(platform)
-	what <- match.arg(what)
+	#what <- match.arg(what)
 
-	if (datatype != "methylation"){
-		stop("Only methylation data are supported for the moment")
+	if (datatype != "methylation" & datatype != "expression"){
+		stop("Only methylation and expression data are supported for the moment")
 	}
-	rgset <- getTCGA.meth(cancer = cancer, platform = platform, verbose = verbose, what = what)
-	rgset
-
+	if (datatype=="methylation"){
+		object <- getTCGA.meth(cancer = cancer, platform = platform, verbose = verbose)
+	} else if (datatype == "expression"){
+		#object <- 
+	}
+	object
 }
 	
 
